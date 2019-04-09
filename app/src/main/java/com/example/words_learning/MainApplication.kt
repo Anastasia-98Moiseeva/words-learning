5bb3be553af587946da6e88fc91aacd4e4336a4e
Tiger20111 <nikit-proc@mail.ru>
2019-04-09 14:16:15 +0100

    sorry

---
+++ app/src/main/java/com/example/words_learning/MainApplication.kt
@@ -0,0 +1,46 @@
+package com.example.words_learning
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.support.v7.app.AppCompatActivity
+import android.util.SparseArray
+import android.widget.Button
+import android.widget.TextView
+import java.lang.StringBuilder
+import kotlin.math.roundToInt
+
+class MainApplication : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_application)
+
+
+    }
+
+
+
+    override fun onResume() {
+        super.onResume()
+
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+
+
+    }
+}
