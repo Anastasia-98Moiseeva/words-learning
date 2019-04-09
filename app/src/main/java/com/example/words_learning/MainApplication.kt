5e45bd6b07b2aab7b1a57aba5a1c48e019873ffb
Tiger20111 <nikit-proc@mail.ru>
2019-04-09 14:19:52 +0100

    sorry

--- app/src/main/java/com/example/words_learning/MainApplication.kt
+++ app/src/main/java/com/example/words_learning/MainApplication.kt
@@ -2,13 +2,7 @@ package com.example.words_learning
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.support.v7.app.AppCompatActivity
-import android.util.SparseArray
-import android.widget.Button
-import android.widget.TextView
-import java.lang.StringBuilder
-import kotlin.math.roundToInt
 
 class MainApplication : AppCompatActivity() {
 
