54ba4741eaaec2dd27ac6c31687489462ed57d71
Tiger20111 <nikit-proc@mail.ru>
2019-04-27 03:22:11 +0100

    adding dictionary

---
+++ app/src/main/java/com/example/words_learning/adapters/CheckedAdapter.kt
@@ -0,0 +1,92 @@
+package com.example.words_learning.adapters
+
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.words_learning.R
+import java.util.ArrayList
+
+
+class SpiritualTeacher(var name: String?, val quote: String, val image: Int) {
+    var isSelected: Boolean = false
+}
+
+
+class CheckedListAdapter(var teachers: Array<SpiritualTeacher>) : RecyclerView.Adapter<CheckedListAdapter.MyHolder>() {
+    var checkedTeachers = ArrayList<SpiritualTeacher>()
+
+    //VIEWHOLDER IS INITIALIZED
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.selected_item, null)
+        return MyHolder(v)
+    }
+
+    //DATA IS BOUND TO VIEWS
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        val teacher = teachers[position]
+        holder.nameTxt.text = teacher.name
+        holder.posTxt.text = teacher.quote
+        holder.myCheckBox.isChecked = teacher.isSelected
+        holder.img.setImageResource(teacher.image)
+
+        holder.setItemClickListener(object : MyHolder.ItemClickListener {
+            override fun onItemClick(v: View, pos: Int) {
+                val myCheckBox = v as CheckBox
+                val currentTeacher = teachers[pos]
+
+                if (myCheckBox.isChecked) {
+                    currentTeacher.isSelected = true
+                    checkedTeachers.add(currentTeacher)
+                } else if (!myCheckBox.isChecked) {
+                    currentTeacher.isSelected = false
+                    checkedTeachers.remove(currentTeacher)
+                }
+            }
+        })
+    }
+
+    override fun getItemCount(): Int {
+        return teachers.size
+    }
+
+    class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        var img: ImageView
+        var nameTxt: TextView
+        var posTxt: TextView
+        var myCheckBox: CheckBox
+
+        lateinit var myItemClickListener: ItemClickListener
+
+        init {
+
+            nameTxt = itemView.findViewById(R.id.nameTextView)
+            posTxt = itemView.findViewById(R.id.descritionTextView)
+            img = itemView.findViewById(R.id.teacherImageView)
+            myCheckBox = itemView.findViewById(R.id.myCheckBox)
+
+            myCheckBox.setOnClickListener(this)
+        }
+
+        fun setItemClickListener(ic: ItemClickListener) {
+            this.myItemClickListener = ic
+        }
+
+        override fun onClick(v: View) {
+            this.myItemClickListener.onItemClick(v, layoutPosition)
+        }
+
+        interface ItemClickListener {
+
+            fun onItemClick(v: View, pos: Int)
+        }
+    }
+
+}
--- app/src/main/java/com/example/words_learning/fragments/dictionary/DictionaryFragment.kt
+++ app/src/main/java/com/example/words_learning/fragments/dictionary/DictionaryFragment.kt
@@ -3,8 +3,6 @@ package com.example.words_learning.fragments.dictionary
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.PagerSnapHelper
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -12,55 +10,36 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.example.words_learning.R
 import com.example.words_learning.Router
-import com.example.words_learning.adapters.ClickableAdapter
-import com.example.words_learning.list.LayoutFragment
-
+import com.example.words_learning.adapters.CheckedListAdapter
+import com.example.words_learning.adapters.SpiritualTeacher
 
 class DictionaryFragment : Fragment() {
 
     private lateinit var router : Router
-    val name = "Dictionary"
-
-    val arrayOfItems = arrayOf(
-        "lorem",
-        "ipsum",
-        "dolor",
-        "sit",
-        "amet",
-        "consectetuer",
-        "adipiscing",
-        "elit",
-        "morbi",
-        "vel",
-        "ligula",
-        "vitae",
-        "arcu",
-        "aliquet",
-        "mollis",
-        "etiam",
-        "vel",
-        "erat",
-        "placerat",
-        "ante",
-        "porttitor",
-        "sodales",
-        "pellentesque",
-        "augue",
-        "purus"
-    )
+    val name = "Words On Topic"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         router = Router(requireActivity(), R.id.fragment_container)
+
     }
 
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         val layout = inflater.inflate(R.layout.fragment_list, container, false)
         val recycler : RecyclerView = layout.findViewById(R.id.list)
 
         recycler.setHasFixedSize(true)
-        createClickableList(recycler)
+
+        recycler.layoutManager = GridLayoutManager(
+            requireContext(),
+            1,
+            RecyclerView.VERTICAL,
+            false
+        )
+        recycler.adapter = CheckedListAdapter(teachers)
 
         val listView = activity!!.findViewById<TextView>(R.id.textView2)
         listView.setText(name)
@@ -68,20 +47,7 @@ class DictionaryFragment : Fragment() {
         return layout
     }
 
-    private fun createClickableList(recycler : RecyclerView) {
-        val layoutManager = GridLayoutManager(
-            requireContext(),
-            2,
-            RecyclerView.VERTICAL,
-            false
-        )
-        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-            override fun getSpanSize(position: Int): Int {
-                return if (position % 3 == 0) 2 else 1
-            }
-        }
-        recycler.layoutManager = layoutManager
-        recycler.adapter = ClickableAdapter(arrayOfItems)
+    private fun onButtonClick(position: Int) {
     }
 
     override fun onResume() {
@@ -90,5 +56,28 @@ class DictionaryFragment : Fragment() {
         listView.setText(name)
     }
 
+
+    companion object {
+        private val teachers: Array<SpiritualTeacher>
+            get() =
+                arrayOf(
+                    SpiritualTeacher("Rumi", "Out beyond ideas of wrongdoing and rightdoing there is a field.I'll meet you there.", R.drawable.icon_loppa),
+                    SpiritualTeacher("Anthony De Mello", "Don't Carry Over Experiences from the past", R.drawable.icon_loppa),
+                    SpiritualTeacher("Eckhart Tolle", "Walk as if you are kissing the Earth with your feet.", R.drawable.icon_loppa),
+                    SpiritualTeacher("Meister Eckhart", "Man suffers only because he takes seriously what the gods made for fun.", R.drawable.icon_loppa),
+                    SpiritualTeacher("Mooji", "I have lived with several Zen masters -- all of them cats.", R.drawable.icon_loppa),
+                    SpiritualTeacher("Confucius", "I'm simply saying that there is a way to be sane. I'm saying that you ", R.drawable.icon_loppa),
+                    SpiritualTeacher("Francis Lucille", "The way out is through the door. Why is it that no one will use this method?", R.drawable.icon_loppa),
+                    SpiritualTeacher("Thich Nhat Hanh", "t is the power of the mind to be unconquerable.", R.drawable.icon_loppa),
+                    SpiritualTeacher("Dalai Lama", "It's like you took a bottle of ink and you threw it at a wall. Smash! ", R.drawable.icon_loppa),
+                    SpiritualTeacher("Jiddu Krishnamurti", "A student, filled with emotion and crying, implored, 'Why is there so much suffering?", R.drawable.icon_loppa),
+                    SpiritualTeacher("Osho", "Only the hand that erases can write the true thing.", R.drawable.icon_loppa),
+                    SpiritualTeacher("Sedata", "Many have died; you also will die. The drum of death is being beaten.", R.drawable.icon_loppa),
+                    SpiritualTeacher("Allan Watts", "Where there are humans, You'll find flies,And Buddhas.", R.drawable.icon_loppa),
+                    SpiritualTeacher("Leo Gura", "Silence is the language of Om. We need silence to be able to reach our Self.", R.drawable.icon_loppa),
+                    SpiritualTeacher("Rupert Spira", "One day in my shoes and a day for me in your shoes, the beauty of travel lies ", R.drawable.icon_loppa),
+                    SpiritualTeacher("Sadhguru", "Like vanishing dew,a passing apparition or the sudden flashnof lightning", R.drawable.icon_loppa)
+                )
+    }
 }
 
