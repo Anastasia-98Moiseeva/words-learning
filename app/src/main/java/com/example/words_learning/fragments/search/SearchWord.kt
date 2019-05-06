70f2292645ebb417c1c303966a6402a7d8ff3717
Anastasia-98Moiseeva <nepro100nastya@yandex.ru>
2019-05-07 02:29:38 +0300

    fix

--- app/src/main/java/com/example/words_learning/fragments/search/SearchWord.kt
+++ app/src/main/java/com/example/words_learning/fragments/search/SearchWord.kt
@@ -12,6 +12,8 @@ import com.example.words_learning.R
 import com.example.words_learning.Router
 import com.example.words_learning.adapters.SearchAdapter
 import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.clickable_item.view.*
+import kotlinx.android.synthetic.main.fragment_search_word.view.*
 import okhttp3.*
 import java.io.IOException
 
@@ -20,6 +22,8 @@ import com.example.words_learning.fragments.request.RequestUrlCreator
 */
 
 class SearchWord : Fragment() {
+
+    var translations: ArrayList<String> = arrayListOf()
 
     private lateinit var router : Router
     val name = "Search"
@@ -34,15 +38,26 @@ class SearchWord : Fragment() {
         var layout = inflater.inflate(R.layout.fragment_search_word, container, false)
 
         layout = createButtons(layout)
+
         val listView = activity!!.findViewById<TextView>(R.id.textView2)
         listView.setText(name)
 
+        //translations.add("sleep")
+        layout.recycle.adapter = SearchAdapter(translations)
         return layout
     }
 
     private fun createButtons(layout: View) : View{
-
+        layout.imageButton2.setOnClickListener(View.OnClickListener {
+            fetchJson(layout.editText.text.toString(), "en-ru")
+            displayTranslations(layout)
+        })
         return layout
+    }
+
+    private fun displayTranslations(layout: View) {
+       //layout.recycle.adapter = SearchAdapter(translations)
+        layout.recycle.adapter?.notifyItemInserted(translations.size - 1)
     }
 
     override fun onResume() {
@@ -51,7 +66,7 @@ class SearchWord : Fragment() {
         listView.setText(name)
     }
 
-    fun fetchJson(word : String, language : String, recycler : RecyclerView) {
+    fun fetchJson(word : String, language : String) {
         val resource : String = "https://translate.yandex.net/api/v1.5/tr.json/translate"
         val apiKey : String = "trnsl.1.1.20190501T234428Z.90b60f7583e9d7b6.ad52ed8f1fb7aa585056ed65c1f9ba9552bc4fd5"
         var url : String = resource + "?key=" + apiKey + "&text=" + word + "&lang=" + language + "&format=plain"
@@ -69,11 +84,12 @@ class SearchWord : Fragment() {
 
                 val homeFeed = gson.fromJson(body, HomeFeed::class.java)
 
-                recycler.adapter = SearchAdapter(homeFeed)
+                translations.add(homeFeed.text[0])
+                //recycler.adapter = SearchAdapter(homeFeed)
 
-                activity!!.runOnUiThread {
-                    recycler.adapter = SearchAdapter(homeFeed)
-                }
+                //activity!!.runOnUiThread {
+                    //recycler.adapter = SearchAdapter(homeFeed)
+                //}
            }
 
             override fun onFailure(call: Call?, e: IOException?){
